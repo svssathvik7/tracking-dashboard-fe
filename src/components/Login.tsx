3a5ee941d7b5350104e7b98989bb81341f6ee36b
SATHVIK SVS <svssathvik77@gmail.com>
2025-04-16 23:16:40 +0530

    add login

---
+++ src/components/Login.tsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useState } from "react";
+import { BACKEND_URL } from "../constants";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    try {
+      const response = (await axios.post(`${BACKEND_URL}/login`)).data;
+      console.log("Loggin successfull ", response);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      <div className="bg-white text-black p-8 rounded-2xl shadow-lg w-full max-w-sm space-y-6">
+        <h1 className="text-2xl font-bold text-center">Login</h1>
+        <input
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="text"
+          placeholder="Email"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+        />
+        <input
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+          placeholder="Password"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+        />
+        <button
+          onClick={() => {
+            handleLogin();
+          }}
+          type="button"
+          className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition"
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "http://0.0.0.0:3000";
